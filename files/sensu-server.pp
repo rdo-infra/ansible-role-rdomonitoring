76c4775b841275f6f10364fd36c0d7ead05562e6
David Moreau Simard <dms@redhat.com>
2016-03-16 15:23:21 -0400

    Try to rely on $::ec2_hostname if it's available

--- files/sensu-server.pp
+++ files/sensu-server.pp
@@ -2,7 +2,16 @@
 include ::rabbitmq
 include ::redis
 
+# OpenStack VMs have a butchered .openstacklocal fqdn
+# Rely on ec2_hostname fact if it's available or fall back to fqdn
+if $::ec2_hostname {
+  $client_name = $::ec2_hostname
+} else {
+  $client_name = $::fqdn
+}
+
 class { 'sensu':
+  client_name   => $client_name,
   subscriptions => ['default', 'master']
 }
 
