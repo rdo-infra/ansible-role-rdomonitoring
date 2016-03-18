4b380d87aa0711a0e177f022d0da331dec89a245
David Moreau Simard <dms@redhat.com>
2016-03-18 09:57:53 -0400

    We shouldn't be filtering resolved event notifications

--- files/filter_check_retry_occurrences.rb
+++ files/filter_check_retry_occurrences.rb
@@ -43,6 +43,12 @@ module Sensu::Extension
     end
 
     def filter(event)
+      # If this is not a new alert but a resolved notification, the filter does
+      # not apply.
+      if event[:action] == "resolve"
+        return ALLOW_PROCESSING, "resolved notifications shouldn't be filtered"
+      end
+
       event_occurrences = event[:occurrences].to_i
       check_occurrences = event[:check][:occurrences].to_i
       retry_occurrences = event[:check][:retry_occurrences].to_i || check_occurrences
